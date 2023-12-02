c0b030745d8a0d1e0d69f14f546e66348edec8fe
veliuysal <veliuysaldev@gmail.com>
2023-12-02 10:38:31 +0300

    cors

--- src/main/java/com/bilgeadam/springboot/SpringbootApplication.java
+++ src/main/java/com/bilgeadam/springboot/SpringbootApplication.java
@@ -2,12 +2,23 @@ package com.bilgeadam.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("http://127.0.0.1:5500");
+            }
+        };
+    }
 
 }
