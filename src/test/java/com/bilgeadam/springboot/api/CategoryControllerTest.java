077a246553a4d1f04bbb06e4a6b1086336a8ace9
veliuysal <veliuysaldev@gmail.com>
2023-12-09 14:11:40 +0300

    Unit test

--- src/test/java/com/bilgeadam/springboot/api/CategoryControllerTest.java
+++ src/test/java/com/bilgeadam/springboot/api/CategoryControllerTest.java
@@ -11,6 +11,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 public class CategoryControllerTest {
 
@@ -46,6 +49,21 @@ public class CategoryControllerTest {
         Category response = categoryController.saveCategory(category);
         Assertions.assertEquals(category.getDescription(), response.getDescription(), "Tanımlamalar yanlış");
         // Mockito.verify(categoryService, Mockito.times(1)).saveCategory(category);
+    }
+
+    @Test
+    public void getAllCategories() {
+        Category category = new Category();
+        category.setDescription("Geliyor Gelmekte Olan");
+        category.setName("Hallederiz");
+        category.setId(11);
+
+        List<Category> returnedList = new ArrayList<>();
+        returnedList.add(category);
+
+        Mockito.when(categoryController.getAllCategories()).thenReturn(returnedList);
+        List<Category> categories = categoryController.getAllCategories();
+        Assertions.assertEquals(1, categories.size(), "Kategoriler 1 tane gelmedi");
     }
 
 }
