b47b84fb6373e12e3d118c88102222c5adc733e5
veliuysal <veliuysaldev@gmail.com>
2023-12-16 12:52:31 +0300

    spring security

--- src/main/java/com/bilgeadam/springboot/api/auth/AuthorizationController.java
+++ src/main/java/com/bilgeadam/springboot/api/auth/AuthorizationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/resource")
+@RequestMapping("/api/resource")
 @RequiredArgsConstructor
 public class AuthorizationController {
     @GetMapping
